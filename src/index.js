6a587df38b87f286fee67c6fa58035002e39f7f1
Swarup <dev@dev-3.local>
2024-06-04 12:26:22 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -17,9 +17,10 @@ root.render(
   </React.StrictMode>,
 );
 
-//Use <BrowserRouter> for server deployment, HashRouter used for github pages*/ 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Use <BrowserRouter> for server deployment, HashRouter used for github pages*/ 
