c84bbd99ef523998725d177c5e5952db5e241a2e
Swarup <dev@dev-3.local>
2024-06-26 16:43:14 +0530

    fix

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -17,17 +17,30 @@ export default function Footer() {
               <br />
               Is Our Calling
             </p>
-          </div>
-          <div className="w-1/3 mt-0 ml-16 mr-0 sm:ml-0 sm:mr-5">
-            <h1 className="text-lg text-theme-blue pt-4 pb-2">
-              Office
-            </h1>
+            {' '}
             <p className="text-lg text-gray-400 font-light">
-              hi@swarup.dev
+              work@swarup.dev
             </p>
             <p className="text-lg text-gray-400 font-light">
               Gobardanga, West Bengal, 743273
             </p>
+          </div>
+          <div className="w-1/3 mt-0 ml-16 mr-0 sm:ml-0 sm:mr-5">
+            <h1 className="text-lg text-theme-blue pt-4 pb-2">
+              Info
+            </h1>
+            <Button href="https://docs.google.com/document/d/e/2PACX-1vQam5xywhVi7sUC-VfmCwCAkuXILkiqjnXRdLbOrcJcUBPHS_mMmhy1r-E5CvhXuUpXzIXl3Q_cZy2G/pub" type="link" target="_blank" className="flex text-lg text-gray-400 font-light hover:underline" isExternal>
+              Terms & Conditions
+            </Button>
+            <Button href="https://docs.google.com/document/d/e/2PACX-1vS5_R4wJwFPrLL0vAKYlTxniTc4-HTA9jZTqlrcoVhIN6cVTu9bndti_x4eDIz1HJl0IUYdLmoqZ4dV/pub" type="link" target="_blank" className="flex text-lg text-gray-400 font-light hover:underline" isExternal>
+              Privacy Policy
+            </Button>
+            <Button href="https://docs.google.com/document/d/e/2PACX-1vSI62yK4YxZG_M2LaqbJ-X7U-Zd_-4Hapvg1sS_YkfenSXMAp_FKxBMXsJEWhl9FJ-R7dH4zGJLoqin/pub" type="link" target="_blank" className="flex text-lg text-gray-400 font-light hover:underline" isExternal>
+              Refund Policy
+            </Button>
+            <Button href="https://docs.google.com/document/d/e/2PACX-1vR-Mvqqkcu1N6RekTZiPyP4j3o88fM7NpN-ifqalxHvuwsRP9mh_Vo-kKkT8m3g_RtkNx_hdbRVmAGv/pub" type="link" target="_blank" className="flex text-lg text-gray-400 font-light hover:underline" isExternal>
+              Contact us
+            </Button>
           </div>
           <div className="w-1/3 ml-16 sm:ml-0 mt-0">
             <h1 className="text-lg text-theme-blue pt-4 pb-2">
